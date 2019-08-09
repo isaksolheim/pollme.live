cef5e0329292779ea4dafabfda886e49b3fd9f62
wickdeeb <isak.m.h.s@gmail.com>
2019-08-09 19:53:44 +0200

    Add footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './styles/main.scss';
 import CreatePoll from './components/CreatePoll';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <CreatePoll />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return(
+    <footer>
+      <p>Created by <a href="https://www.isaks.io/">isaks.io</a></p>
+    </footer>
+  );
+}
+
+export default Footer;
