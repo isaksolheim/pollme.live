c4b5fbbd05326ff02a1600941e94eebe9decf8bf
wickdeeb <isak.m.h.s@gmail.com>
2019-08-09 21:45:33 +0200

    Add frontend/backend communication

--- src/components/CreatePoll.js
+++ src/components/CreatePoll.js
@@ -20,9 +20,18 @@ class CreatePoll extends React.Component {
   newPollHandler = (e) => {
     e.preventDefault();
 
+    let options = [];
+    for (var i = 0; i < document.getElementById('options').children.length; i++) {
+     let option = {
+      "content": document.getElementById('options').children[i].children[1].value,
+      "votes": 0
+      };
+      options.push(option);
+    }
+
     var poll = {
       "question": this.state.question,
-      "options": this.state.options
+      "options": options 
     };
 
     axios.post('http://localhost:5000/create', poll)
@@ -50,11 +59,11 @@ class CreatePoll extends React.Component {
     for (var i = 1; i <= this.state.numOptions; i++) {
       let newOption = (
         <div key={i} className="option">
-          <label onClick={this.deleteOption}>
+          <label>
             Option {i}
             {i === this.state.numOptions ? <i onClick={this.deleteOption} className="far fa-trash-alt" /> : null}
           </label>
-          <input type="text" name="question" onChange={this.inputHandler} />
+          <input type="text" name="option" onChange={this.inputHandler} />
         </div>
       );
 
@@ -75,7 +84,7 @@ class CreatePoll extends React.Component {
             {this.options()}
           </div>
           <button onClick={this.addOption}>Add Option</button>
-          <input type="submit" value="Submit" />
+          <input type="submit" value="Create Poll" />
         </form>
       </section>
     );
