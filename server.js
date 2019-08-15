646f4b46cc2286584cf005309882f826acf1d214
wickdeeb <isak.m.h.s@gmail.com>
2019-08-15 13:14:40 +0200

    Change project file structure

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require('path');
 
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('../build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
+  });
+}
 
 app.use(cors());
 app.use(express.json());
